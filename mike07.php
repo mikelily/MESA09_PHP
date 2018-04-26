663bb21a35ad3698a3b439a02cf42a86842a27d8
mikelily <mikelily@gmail.com>
2018-04-26 10:15:35 +0800

    20180426 v2

--- mike07.php
+++ mike07.php
@@ -1,31 +1,38 @@
 <?php
-    $ans = $x = $y =""; //預設值 string
+    $ans = $x = $y = $op =""; //預設值 string
+
     if(isset($_GET['x']) && isset($_GET['y'])) {
         $x = $_GET['x'];
-        //    echo $x . ':' . gettype($x). "<br>";
         $y = $_GET['y'];
-        //    var_dump($y);
-        //    echo "<br>";
-        //    echo $y . ':' . gettype($y);
-        //    echo "<br>";
-        $ans = $x + $y; //integer
-//        echo "{$x} + {$y} = {$ans}";
+        $op = $_GET['op'];
+
+
+        switch ($op) {
+            case 1:
+                $ans = $x + $y;
+                break;
+            case 2:
+                $ans = $x - $y;
+                break;
+            case 3:
+                $ans = $x * $y;
+            case 4:
+                $ans = $x / $y;
+                break;
+        }
     };
 ?>
 <form>
     <input type = "text" name = "x" value="<?php echo $x; ?>"/>
-    <select>
-        <option>+</option>
-        <option>-</option>
-        <option>*</option>
-        <option>/</option>
+    <select name="op">
+        <option value="1">+</option>
+        <option value="2">-</option>
+        <option value="3">*</option>
+        <option value="4">/</option>
     </select>
     <input type = "text" name = "y" value="<?php echo $y; ?>"/>
     <input type = "submit" value = "="/>
     <?php
-//        if(isset($_GET['x']) && isset($_GET['y'])) {
-//            echo $ans;
-//        };
         echo $ans;
     ?>
 </form>
