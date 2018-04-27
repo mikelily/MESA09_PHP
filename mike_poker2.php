09528767d8103689213fd2c81ae8a87c5222e837
mikelily <mikelily@gmail.com>
2018-04-27 17:30:53 +0800

    20180426 v1

--- mike_poker2.php
+++ mike_poker2.php
@@ -13,3 +13,30 @@
         $players[$i%4][] = $p;
     }
 
+?>
+
+<table border="1" width="100%">
+        <?php
+            $suits = array('&spades;','&hearts;','&clubs;','&diams;');
+            $v = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+            foreach ($players as $player) {
+                echo "<tr>";
+                foreach ($player as $p) {
+                    echo "<td><font color='";
+                    if ((int)($p/13)%4==1){
+                        echo "red' >";
+                        //echo "<td>{$suits[(int)($p/13)]}{$v[$p%13]}</td>";
+                        echo "{$suits[(int)($p/13)]}{$v[$p%13]}";
+                    }else {
+                        echo "black' >";
+                        echo "{$suits[(int)($p/13)]}{$v[$p%13]}";
+                    }
+//                        echo "{$suits[(int)($p/13)]} {$card13[$p%4]}";
+
+                    echo "</font></td>";
+                    //echo "<td>{$p}</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+</table>
