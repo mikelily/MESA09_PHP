b8fd69533924ec9ffd1ab3efe3e8f24084ddafbd
mikelily <mikelily@gmail.com>
2018-05-01 20:10:57 +0800

    20180430 v1

---
+++ HW_poker.php
@@ -0,0 +1,30 @@
+<?php
+    //用交換的方式洗牌
+
+    //給一副沒洗過的牌
+    for($i=0;$i<52;$i++){
+        $poker[$i] = $i;
+    };
+    for($i=0;$i<52;$i++){
+        if($i==51)
+            break;
+        else{
+            $rand = rand($i+1,51);
+            $tmp =0;
+            $tmp = $poker[$i];
+            $poker[$i] = $poker[$rand];
+            $poker[$rand] = $tmp;
+        }
+    };
+    //    print
+    $forBr = 0;
+    foreach ($poker as $v){
+        if($forBr==13){
+            echo '<br>';
+            $forBr = 0;
+        }
+        echo "{$v} ";
+        $forBr++;
+    };
+?>
+
