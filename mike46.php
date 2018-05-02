8e4d1e1b724eed6d8ecca5d748b5b5514eec9615
mikelily <mikelily@gmail.com>
2018-05-02 11:30:11 +0800

    Check Prime Hw Done

---
+++ mike46.php
@@ -0,0 +1,6 @@
+<h1>Mike Big Company</h1>
+<hr>
+Mike : <img src="mike45.php?rate=70" /><br>
+Tony : <img src="mike45.php?rate=30" /><br>
+Andy : <img src="mike45.php?rate=94.87" /><br>
+Cany : <img src="mike45.php?rate=44.32" /><br>
